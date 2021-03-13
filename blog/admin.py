d850112986af72b7f973e9137a528065cbd837fe
nikolaj <strashevskiynn@g,ail.com>
2021-03-13 13:27:23 +0300

    модель books и ее отображение в админке

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Books
+
+
+@admin.register(Books)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'public', 'date_add', 'id')
+    fields = ('date_add', ('title', 'public'), 'message')
+    readonly_fields = ('date_add', )
+    search_fields = ['title', 'message', ]
+    list_filter = ('public', )
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Books(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    message = models.TextField(default='', verbose_name='Текст')
+    date_add = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    public = models.BooleanField(default=False, verbose_name='Опубликовать')
+
+    def __str__(self):
+        return self.title
 # Create your models here.
